2718b4bc7fe73b0f72052e39af6d960a09957333
Aleksandr Saiyan <anverch@gmail.com>
2020-05-24 15:04:46 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const fs = require("fs")
+const express = require("express");
+
+// Tells node that we are creating an "express" server
+let app = express();
+
+// Sets an initial port. 
+let PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// The below points our server to a series of "route" files.
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER
+// The below code effectively "starts" our server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
